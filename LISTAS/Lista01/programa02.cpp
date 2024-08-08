4821f7203407d0d21d1aafc80a6352e55a64a8a5
andre luiz <andreluiz456654@gmail.com>
2024-08-08 16:37:26 -0300

    meu primeiro commit

---
+++ LISTAS/Lista01/programa02.cpp
@@ -0,0 +1,11 @@
+// Programa 02
+// Lista 01 programa02.cpp
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Informe um numero "; cin >> num;
+    cout << "O numero informado foi "<< num <<"."<< endl;
+    return 0;
+}
