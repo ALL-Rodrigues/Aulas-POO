8a81ecd14a1211395fef5ccd2c25196b902a49ef
andre luiz <andreluiz456654@gmail.com>
2024-08-08 21:48:35 -0300

    meu primeiro commit

---
+++ LISTAS/Lista01/programa03.cpp
@@ -0,0 +1,17 @@
+// Programa 03
+// Lista 01 programa03.cpp
+
+#include<iostream>
+using namespace std;
+int main(){
+    float n1, n2, n3, n4, tl, mf;
+    cout << "informe notas do aluno:" <<endl;
+    cout << "Nota 1: "; cin >> n1;
+    cout << "Nota 2: "; cin >> n2;
+    cout << "Nota 3: "; cin >> n3;
+    cout << "Nota 4: "; cin >> n4;
+    tl =  n1 + n2 + n3 + n4;
+    mf = tl/4;
+    cout << "Media Final e "<< mf << endl;
+    return 0;
+}
